6bd7f60512b4c917fb6e1f6b059ae41060cf9bea
hansai <18611124622@163.com>
2019-05-13 13:58:32 +0800

    hahahah

--- microservicecloud_consumer_dept_80/src/main/java/com/hansai/springcloud/config/RestConfig.java
+++ microservicecloud_consumer_dept_80/src/main/java/com/hansai/springcloud/config/RestConfig.java
@@ -1,5 +1,7 @@
 package com.hansai.springcloud.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,9 +19,22 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
 
+    /**
+     * restTemplate 调用restful 接口 ,@LoadBalanced 表示启用ribbon负载均衡
+     * @return
+     */
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+    }
+
+    /**
+     * ribbon 负载策略
+     * @return
+     */
+    @Bean
+    public IRule iRule() {
+        return new RandomRule();
     }
 }
